a8aad336a159c2aeeef5c4d9afebe5b652306f78
veeranjaneya45 <pp0238@Srmist.edu.in>
2025-08-03 23:30:39 +0530

    Create 1.C

---
+++ 1.C
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {5, 2, 9, 1, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int i, j, temp;
+
+  
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+
+    printf("Sorted array: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
